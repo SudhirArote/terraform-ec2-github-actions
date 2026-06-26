b8ed51badd6b920bccfa9c5e25530aa58e792aa4
SudhirArote <sudhirarote10@outlook.com>
2026-06-26 16:46:04 +0530

    added var

--- providers.tf
+++ providers.tf
@@ -1,5 +1,16 @@
 terraform {
-  required_providers { aws={source="hashicorp/aws" version="~> 5.0"} }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
 }
-provider "aws" { region=var.region }
-variable "region" {default="us-east-1"}
+
+provider "aws" {
+  region = var.region
+}
+
+variable "region" {
+  default = "ap-south-1"
+}
